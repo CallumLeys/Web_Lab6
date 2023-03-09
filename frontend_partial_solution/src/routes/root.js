11beb09b572878994a3cd8afc2ff558bab4c8c2b
CALLUM LEYS (1802490) <c.leys@rgu.ac.uk>
2023-03-09 13:20:08 +0000

    adding frontend

---
+++ frontend_partial_solution/src/routes/root.js
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom";
+
+export default function Root() {
+    return (
+      <>
+        <div id="sidebar">
+          <h1>Menu</h1>
+          <div>
+            <a href={'/login'}>Login</a>
+          </div>
+          <div>
+            <a href={'/signup'}>Sign up</a>
+          </div>
+        </div>
+        <br></br>
+        <div id="detail">
+            <Outlet />
+        </div>
+      </>
+    );
+  }
